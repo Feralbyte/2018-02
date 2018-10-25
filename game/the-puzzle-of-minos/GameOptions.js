add6dbf7010723be460eb802562ffbf87a4b9306
Eduardo Messias de Andrade e Oliveira <eduardo.messias@outlook.com>
2018-10-25 21:24:47 +0200

    Delete GameOptions.js

--- game/the-puzzle-of-minos/GameOptions.js
+++
@@ -1,9 +0,0 @@
-class GameOptions {
-    constructor() {
-        this.width  = 340;
-        this.height = 480;
-        this.type   = Phaser.AUTO;
-        this.key    = 'ThePuzzleOfMinos';
-        this.states = [ new GameStateInfo('TheseusShipArrives', TheseusShipArrives, false)];
-    }
-}
