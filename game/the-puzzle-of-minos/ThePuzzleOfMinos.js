9ce1b54b88ed80d3f4697f4b045fb03db85f66c5
Eduardo Messias de Andrade e Oliveira <eduardo.messias@outlook.com>
2018-10-25 21:24:28 +0200

    Delete ThePuzzleOfMinos.js

--- game/the-puzzle-of-minos/ThePuzzleOfMinos.js
+++
@@ -1,9 +0,0 @@
-class ThePuzzleOfMinos extends Phaser.Game {
-    constructor(options) {
-        super(options.width, options.height, options.type, options.key);
-
-        options.states.foreach(function(gameStateInfo) {
-            this.state.add(gameStateInfo.key, gameStateInfo.state, gameStateInfo.autoStart);
-        });
-    }
-}
