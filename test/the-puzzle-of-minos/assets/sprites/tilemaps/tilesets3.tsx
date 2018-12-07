7cac966c02f043c47354ec71ef238744c1d27b7c
Eduardo Messias de Andrade e Oliveira <eduardo.messias@outlook.com>
2018-12-08 00:06:27 +0100

    Add files via upload

---
+++ test/the-puzzle-of-minos/assets/sprites/tilemaps/tilesets3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="Tilesets3" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="Graficos Tilesets/Tiles B/gallery_57043_149_19348.png" width="512" height="512"/>
+</tileset>
